7a07a38865abda6078efb0cec36fe3470d770d04
zeecnla <csrmelchor@gmail.com>
2017-10-22 12:36:57 -0700

    Assertion
    Added string encode and base64encode

--- Solutions/Set-01/Challenge-01.py
+++ Solutions/Set-01/Challenge-01.py
@@ -1,12 +1,15 @@
 # Set 1 Challenge 1
 # Convert Hex to base64
-import binascii
+import binascii, base64
 print("Set 1 - Challenge 1: ")
 
 answer = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
-hex_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-bytes_string = binascii.unhexlify(hex_string)
-message = binascii.b2a_base64(bytes_string)
+input_ = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 
-print(message.decode("utf-8"));
-assert(message == answer)
+encoded_string = str.encode(input_);
+input_bytes = binascii.unhexlify(encoded_string)
+output = base64.b64encode(input_bytes)
+
+print("Input: \n"+input_)
+print("Output: \n"+output)
+assert(output == answer)
