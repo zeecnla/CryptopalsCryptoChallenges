709fa8edf42a1cb7937b130d9fbe81776bc6000a
zeecnla <csrmelchor@gmail.com>
2017-10-23 23:28:33 -0700

    Formated Hex
    Output has hex format with 0x in the beginning removed it

--- Solutions/Set-01/Challenge-02.py
+++ Solutions/Set-01/Challenge-02.py
@@ -1,10 +1,17 @@
+
 # Set 1 Challenge 2
 # Fixed XOR
+import binascii, base64
 print("Set 1 - Challenge 2: ")
-def XOROperation(a, b):
-    result = hex(int(a,16)^ int(b, 16))
-    return result
 
-the_string =    "1c0111001f010100061a024b53535009181c"
-second_string = "686974207468652062756c6c277320657965"
-print(XOROperation(the_string, second_string))
+
+def XOROperation(a, b):
+    output = int(a,16) ^ int(b,16)
+    return format(output, 'x')
+
+s1 = "1c0111001f010100061a024b53535009181c"
+s2 = "686974207468652062756c6c277320657965"
+answer = "746865206b696420646f6e277420706c6179"
+if len(s1) == len(s2):
+    output = XOROperation(s1, s2)
+    assert(answer==output)
