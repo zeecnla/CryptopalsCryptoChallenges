098dee01954b6502d8343e5b0dea2869c95db841
Cesar Melchor <csrmelchor@gmail.com>
2018-01-03 17:33:25 -0800

    Hammin distance
    Finished the hamming distance  method. need to start normalizeing the key length

--- Solutions/Set-01/Challenge06.py
+++ Solutions/Set-01/Challenge06.py
@@ -1,11 +1,26 @@
+#Break repeating key XOR
 import base64
 import binascii
 
 
-
-
+def Hamming_Distance(a,b):
+    assert len(a)==len(b)
+    a = ''.join([bin(ord(x))[2:].zfill(8) for x in a])
+    b = ''.join([bin(ord(x))[2:].zfill(8) for x in b])
+    i = 0
+    counter = 0
+    while i != len(a):
+        if(a[i] != b[i]):
+            counter = counter + 1
+        i = i+1
+    return counter
+  
 def Break_Repeating_XOR():
-
+    
+    #Length of the key
+    keysize = (list(bytes(range(2,41))))
+    print(keysize)
     file_path = "H:\Projects\CryptopalsCryptoChallenges\Solutions\Set-01\\assets\\6.txt"    
-    with open(file_path, "r") as f:
+    #with open(file_path, "r") as f:
      
+Break_Repeating_XOR()
